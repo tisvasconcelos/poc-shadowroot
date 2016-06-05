b4aabe9439572a112bfe3ce3d44c19a3e1947f17
Tiago Vasconcelos <tisvasconcelos@gmail.com>
2016-06-05 16:44:20 -0300

    Fix style scope

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 define(['doc'], function($){
 	var proto = Object.create(HTMLElement.prototype);
 	proto.createdCallback = function(){
+		var children = this.children;
 		var root = this.createShadowRoot();
-		for (var i = 0, max = this.children.length; i < max; i++) {
-			var child = this.children[i];
+		for (var i = 0, max = children.length; i < max; i++) {
+			var child = children[i];
 			root.appendChild(child.cloneNode(true));
 		}
+		this.removeChild(this.querySelector('*'));
 	};
 	var wc = document.registerElement('web-component', {
 		prototype: proto
